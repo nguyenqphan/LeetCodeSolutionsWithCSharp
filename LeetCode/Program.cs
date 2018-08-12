09423b283246849b6b0a6531327d1d8276b854a7
Nguyen Phan <nqphan212@gmail.com>
2018-08-11 22:48:29 -0400

    Two Sum

---
+++ LeetCode/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+         
+        }
+
+
+        /* TWO SUME
+         * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+        You may assume that each input would have exactly one solution, and you may not use the same element twice.*/
+       
+        static int[] TwoSum(int[] nums, int target)
+        {
+
+            int[] result = new int[2];
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int complement = target - nums[i];
+                if (dic.ContainsKey(complement))
+                {
+                    result[0] = dic[complement];
+                    result[1] = i;
+                    return result;
+                }
+                if (!dic.ContainsKey(nums[i]))
+                {
+                    dic.Add(nums[i], i);
+                }
+            }
+
+            return result;
+        }
+
+    }
+
+
+}
