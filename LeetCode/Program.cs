fc02f936724ff4139c99ad227f1fdd13b86e2170
Nguyen Phan <nqphan212@gmail.com>
2019-03-27 09:11:33 -0400

    Find the middle of a linked link

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -760,5 +760,24 @@ namespace LeetCode
             return true;
         }
 
+
+        //MIDDLE OF THE LINED LIST
+        //Given a non-empty, singly linked list with head node head, 
+        //return a middle node of linked list.
+        //If there are two middle nodes, return the second middle node.
+        public ListNode MiddleNode(ListNode head)
+        {
+
+            ListNode fast = head;
+
+            while (fast != null && fast.next != null)
+            {
+                head = head.next;
+                fast = fast.next.next;
+            }
+
+            return head;
+        }
+
     }//close program class
 }//close name space
