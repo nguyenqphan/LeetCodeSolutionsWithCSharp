88bc5d3f1b4e4516c6cf0d86762f2da01cda003e
Nguyen Phan <nqphan212@gmail.com>
2019-04-03 23:44:33 -0400

    Pascal's Triangle

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -15,6 +15,37 @@ namespace LeetCode
 
         }
 
+        //PASCAL'S TRIANGLE
+        //Given a non-negative integer numRows, generate the first numRows of Pascal's triangle.
+        //In Pascal's triangle, each number is the sum of the two numbers directly above it.
+        public IList<IList<int>> Generate(int numRows)
+        {
+            IList<IList<int>> nums = new List<IList<int>>();
+
+            for (int i = 1; i <= numRows; i++)
+            {
+                IList<int> newRow = new List<int>();
+                for (int j = 1; j <= i; j++)
+                {
+                    if (j == 1 || j == i)
+                    {
+                        newRow.Add(1);
+                    }
+                    else
+                    {
+                        newRow.Add(nums[i - 2][j - 2] + nums[i - 2][j - 1]);
+                    }
+                }
+
+                nums.Add(newRow);
+
+            }
+
+            return nums;
+
+
+        }
+
         //BEST TIME TO BUY AND SELL STOCK
         /* Say you have an array for which the ith element is the price of a given stock on day i.
          * If you were only permitted to complete at most one transaction
