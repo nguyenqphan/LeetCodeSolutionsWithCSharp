fbae8e72cd9283ae4cf72896037dccfc27e9443c
Nguyen Phan <nqphan212@gmail.com>
2019-04-08 17:07:18 -0400

    Merge two binary trees

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -12,6 +12,29 @@ namespace LeetCode
            
         }
 
+        //MERGE TWO TREES
+        //Given two binary trees and imagine that when you put one of them to cover the other, 
+        //some nodes of the two trees are overlapped while the others are not.
+        //You need to merge them into a new binary tree. The merge rule is that if two nodes overlap, 
+        //then sum node values up as the new value of the merged node. Otherwise,
+        //the NOT null node will be used as the node of new tree.
+        public static TreeNode MergeTrees(TreeNode t1, TreeNode t2)
+        {
+            if (t1 == null)
+                return t2;
+
+            if (t2 == null)
+                return t1;
+
+            t1.val += t2.val;
+
+            t1.left = MergeTrees(t1.left, t2.left);
+            t1.right = MergeTrees(t1.right, t2.right);
+
+            return t1;
+
+        }
+
         //SYMMETRIC TREE (iterative solution)
         //Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
         public static bool IsSymmetric2(TreeNode root)
