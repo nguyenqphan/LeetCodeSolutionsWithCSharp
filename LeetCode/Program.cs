e4eebf65bcd520a8d42ffd8669aeb71be8d43e5b
Nguyen Phan <nqphan212@gmail.com>
2020-02-05 18:28:27 -0500

    Two Sum II - Input Array is Sorted - Easy

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -14,6 +14,33 @@ namespace LeetCode
            
         }
 
+        //TWO SUM II - INPUT ARRAY IS SORTED
+        //Given an array of integers that is already sorted in ascending order,
+        //find two numbers such that they add up to a specific target number.
+        //The function twoSum should return indices of the two numbers such that
+        //they add up to the target, where index1 must be less than index2.
+        //Note:
+        //Your returned answers (both index1 and index2) are not zero-based.
+        //You may assume that each input would have exactly one solution and
+        //you may not use the same element twice.
+        public static int[] TwoSum(int[] numbers, int target)
+        {
+            int first = 0;
+            int last = numbers.Length - 1;
+
+            while (first < last)
+            {
+                if (numbers[first] + numbers[last] > target)
+                    last--;
+                else if (numbers[first] + numbers[last] < target)
+                    first++;
+                else
+                    break;      //muss be equal to the target because one solution is guaranteed
+            }
+
+            return new int[] { first + 1, last + 1 };
+        }
+
         //PATH SUM
         //Given a binary tree and a sum, determine if the tree has
         //a root-to-leaf path such that adding up all the values along
