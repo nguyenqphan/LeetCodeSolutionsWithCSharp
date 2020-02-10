609877909a4d12c594efaffab9e8d29adfadd5f2
Nguyen Phan <nqphan212@gmail.com>
2020-02-10 18:05:24 -0500

    Perfect Square - Easy

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -13,6 +13,30 @@ namespace LeetCode
             
         }
 
+        //Perfect Square
+        public static bool IsPerfectSquare(int num)
+        {
+            if (num == 1)
+                return true;
+
+            long left = 1;
+            long right = num / 2;
+
+            while (left <= right)
+            {
+                long mid = left + (right - left) / 2; //overflow if int is used here
+
+                if (mid * mid == num)
+                    return true;
+                else if (mid * mid < num)
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+
+            return false;
+        }
+
         //COUNT AND SAY (Refer to LeetCode for the problem discription)
         public static string CountAndSay(int n)
         {
